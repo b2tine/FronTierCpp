94d50547cf68762246912fc62481cfd1da7f573e
Brandon <brandon.ballentine@stonybrook.edu>
2018-10-06 12:58:22 -0400

    googletest.1.8.0 packaged with FronTier. I've been able to compile the static libraries, but I still need to set up the actual testing environment in the directory "tests"

--- autogen.sh
+++ autogen.sh
@@ -1,8 +1,5 @@
 #!/bin/sh
 
-export LD_LIBRARY_PATH="/usr/local/hdf4-2.10/lib"
-
-autoreconf --verbose --install --force
+autoreconf -vif
 ./configure --with-hdf4-dir=/usr/local/hdf4-2.10
-make clean
 make
