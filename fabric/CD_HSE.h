522922578aa44da06a6f3b406d1b7e0ccf4e87d9
Brandon <brandon.ballentine@stonybrook.edu>
2020-03-02 13:48:50 -0500

    const correctness CD_HSE files

--- fabric/CD_HSE.h
+++ fabric/CD_HSE.h
@@ -14,44 +14,21 @@ enum class CD_HSE_TYPE
     RIGID_TRI
 };
 
-
-//abstract base class for hypersurface element(HSE)
-//can be a point, bond, or triangle
 struct CD_HSE
 {
     CD_HSE_TYPE type;
-	virtual double max_static_coord(int) = 0;
-	virtual double min_static_coord(int) = 0;
-	virtual double max_moving_coord(int,double) = 0;
-	virtual double min_moving_coord(int,double) = 0;
-	virtual POINT* Point_of_hse(int) const  = 0;
-	virtual int num_pts() const= 0;
+	virtual double max_static_coord(int dim) const = 0;
+	virtual double min_static_coord(int dim) const = 0;
+	virtual double max_moving_coord(int dim, double dt) const = 0;
+	virtual double min_moving_coord(int dim, double dt) const = 0;
+	virtual POINT* Point_of_hse(int i) const  = 0;
+	virtual int num_pts() const noexcept = 0;
 	virtual ~CD_HSE(){};
 };
 
-//wrap class for triangle
-struct CD_TRI: public CD_HSE
-{
-    TRI* m_tri;
-	
-    CD_TRI(TRI* tri, CD_HSE_TYPE tag)
-        : m_tri{tri}
-    {
-        type = tag;
-    }
-
-	double max_static_coord(int);
-	double min_static_coord(int);
-	double max_moving_coord(int,double);
-	double min_moving_coord(int,double);
-	POINT* Point_of_hse(int) const;
-	int num_pts() const {return 3;}
-};
-
-//wrap class for bond
 struct CD_BOND: public CD_HSE
 {
-	int m_dim;
+	int m_dim; //remove
     BOND* m_bond;
 	
     CD_BOND(BOND* bond, CD_HSE_TYPE tag)
@@ -68,36 +45,34 @@ struct CD_BOND: public CD_HSE
         }
     }
 
-	double max_static_coord(int);
-	double min_static_coord(int);
-	double max_moving_coord(int,double);
-	double min_moving_coord(int,double);
-	POINT* Point_of_hse(int) const;
-	int num_pts()const{return 2;}
+	double max_static_coord(int dim) const override;
+	double min_static_coord(int dim) const override;
+	double max_moving_coord(int dim, double dt) const override;
+	double min_moving_coord(int dim, double dt) const override;
+	POINT* Point_of_hse(int i) const override;
+	int num_pts() const noexcept override {return 2;}
 };
 
-/*
-//wrap class for point
-struct CD_POINT: public CD_HSE
+struct CD_TRI: public CD_HSE
 {
-    POINT* m_point;
+    TRI* m_tri;
+	
+    CD_TRI(TRI* tri, CD_HSE_TYPE tag)
+        : m_tri{tri}
+    {
+        type = tag;
+    }
 
-    CD_POINT(POINT* point)
-        : m_point(point)
-    {}
-
-	double max_static_coord(int);
-	double min_static_coord(int);
-	double max_moving_coord(int,double);
-	double min_moving_coord(int,double);
-	POINT* Point_of_hse(int) const;
-	int num_pts()const {return 1;}
+	double max_static_coord(int dim) const override;
+	double min_static_coord(int dim) const override;
+	double max_moving_coord(int dim, double dt) const override;
+	double min_moving_coord(int dim, double dt) const override;
+	POINT* Point_of_hse(int i) const override;
+	int num_pts() const noexcept override {return 3;}
 };
-*/
 
 
 bool adjacentHSE(CD_HSE* A, CD_HSE* B);
-
 
 
 #endif
