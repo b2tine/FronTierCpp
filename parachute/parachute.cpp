ed200ac4b2a454ef8a06473dce53c3994f8cb382
brandon <brandon.ballentine@stonybrook.edu>
2020-09-30 13:56:36 -0400

    not yet in use but added (commented out) front->dt = std::min(front->dt,springCharTimeStep(front)); into the next dt computation of the driver

--- parachute/parachute.cpp
+++ parachute/parachute.cpp
@@ -340,8 +340,13 @@ void airfoil_driver(Front *front,
         }
         
         if (!af_params->no_fluid)
+        {
             front->dt = std::min(front->dt,CFL*l_cartesian->max_dt);
-   
+            //TODO:necessary?
+            //
+            //front->dt = std::min(front->dt,springCharTimeStep(front));
+        }
+
         if (debugging("step_size"))
         {
             printf("Time step from l_cartesian->max_dt(): %f\n",front->dt);
