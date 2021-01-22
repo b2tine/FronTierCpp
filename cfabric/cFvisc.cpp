7c871eddd39ec64612b5ccad38a5db9de1534241
brandon <brandon.ballentine@stonybrook.edu>
2021-01-21 23:52:21 -0500

    setting up function skeletons for viscous flux computations

--- cfabric/cFvisc.cpp
+++ cfabric/cFvisc.cpp
@@ -1,4 +1,5 @@
-#include "cFvisc.h"
+//#include "cFvisc.h"
+#include "cFluid.h"
 
 
 
@@ -22,19 +23,17 @@ void G_CARTESIAN::addViscousFlux(
         FSWEEP *m_flux,
         double delta_t)
 {
-    //for (int k = imin[2]; k <= imax[2]; k++)
+    for (int k = imin[2]; k <= imax[2]; k++)
     for (int j = imin[1]; j <= imax[1]; j++)
     for (int i = imin[0]; i <= imax[0]; i++)
-    {
-        int index = d_index3d(i,j,k,top_gmax);
-        if (!gas_comp(top_comp[index]))
-        {
-            //zero state vals if necessary
-            continue;
-        }
-
+    {   
         int icoords[MAXD] = {i,j,k};
         computeViscousFlux(icoords,m_vst,m_flux,delta_t);
+        //TODO: Could have computeViscousFlux() return a
+        //      FSWEEP like structure which we then add the 
+        //      contents of to m_flux here. More faithful
+        //      implementation to the function name, and allows
+        //      for single responsibility.
     }
     
 }
@@ -46,8 +45,25 @@ void G_CARTESIAN::computeViscousFlux(
         FSWEEP *m_flux,
         double delta_t)
 {
-    GRID_DIRECTION ldir[3] = {WEST,SOUTH,LOWER};
-    GRID_DIRECTION rdir[3] = {EAST,NORTH,UPPER};
+    int index = d_index3d(i,j,k,top_gmax);
+    COMPONENT comp = top_comp[index];
+    if (!gas_comp(comp))
+    {
+        return;
+    }
+
+    POINTER state;
+    HYPER_SURF* hs;
+        //HYPER_SURF_ELEMENT* hse;
+    double crx_coords[MAXD];
+    int crx_status;
+
+    const GRID_DIRECTION dir[3][2] = {
+        {WEST,EAST},{SOUTH,NORTH},{LOWER,UPPER}
+    };
+
+    
+    //TODO: create FSWEEP var and alloc data
 
     //From grid point i,j,k need to check for interface crossings
     //between immediate neighbors. Will then need to move to each
@@ -60,8 +76,20 @@ void G_CARTESIAN::computeViscousFlux(
         for (int nb = 0; nb < dim; ++nb)
         {
     
-            //FT_StateStructAtGridCrossing();
+            /*
+            FT_StateStructAtGridCrossing(front,front->grid_intfc,
+                    icoords,dir[l][nb],comp,&intfc_state,&hs,crx_coords);
+            */
+            
+                /*
+                FT_StateStructAtGridCrossing2(front,icoords,dir[l][nb],
+                        comp,&intfc_state,&hs,&hse,crx_coords);
+                */
         }
 
     }
+
+
+    //TODO: Add computed flux to m_flux, or return an FSWEEP like structure.
+
 }
