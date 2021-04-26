9197dfe2bc0681def93dc027e85dae0d73fe6b78
brandon <brandon.ballentine@stonybrook.edu>
2021-04-26 16:46:21 -0400

    input-brandon-VREMAN name changes

--- parachute/input-brandon-VREMAN/run-c9-vreman.sh
+++ parachute/input-brandon-VREMAN/run-c9-vreman.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-./parachute -d 3 -i in-C9-VnG-ball-VREMAN-v15 -o out-vng-ball-vreman-v15 &
-./parachute -d 3 -i in-C9-VnG-box-VREMAN-v15 -o out-vng-box-vreman-v15 &
-./parachute -d 3 -i in-C9-VnG-human-VREMAN-v15 -o out-vng-human-vreman-v15 &
+./parachute -d 3 -i in-C9-VnG-ball-VREMAN-v5 -o out-vng-ball-vreman-v15 &
+./parachute -d 3 -i in-C9-VnG-box-VREMAN-v5 -o out-vng-box-vreman-v15 &
+./parachute -d 3 -i in-C9-VnG-human-VREMAN-v5 -o out-vng-human-vreman-v15 &
