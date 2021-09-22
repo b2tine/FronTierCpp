37b36d18aadb2a9230ba20ebb7e6d4f974ae9660
brandon <brandon.ballentine@stonybrook.edu>
2021-09-22 06:03:25 -0400

    input-brandon-FWE run script

---
+++ parachute/input-brandon-FWE/run-brandon-FWE.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+INPUTDIR="input-brandon-FWE"
+
+nohup ./parachute -d 3 -i ${INPUTDIR}/in-C9-nVnG-pointmass-VREMAN-v12 -o out-C9-nVnG-pointmass-VREMAN-v12 &
+nohup ./parachute -d 3 -i ${INPUTDIR}/in-C9-nVnG-ball-VREMAN-v12 -o out-C9-nVnG-ball-VREMAN-v12 &
+nohup ./parachute -d 3 -i ${INPUTDIR}/in-C9-nVnG-box-VREMAN-v12 -o out-C9-nVnG-box-VREMAN-v12 &
+nohup ./parachute -d 3 -i ${INPUTDIR}/in-C9-nVnG-human-VREMAN-v12 -o out-C9-nVnG-human-VREMAN-v12 &
+
+nohup ./parachute -d 3 -i ${INPUTDIR}/in-C9-nVnG-fixedpointmass-VREMAN-v12 -o out-C9-nVnG-fixedpointmass-VREMAN-v12 &
+nohup ./parachute -d 3 -i ${INPUTDIR}/in-C9-nVnG-fixedball-VREMAN-v12 -o out-C9-nVnG-fixedball-VREMAN-v12 &
+nohup ./parachute -d 3 -i ${INPUTDIR}/in-C9-nVnG-fixedbox-VREMAN-v12 -o out-C9-nVnG-fixedbox-VREMAN-v12 &
+nohup ./parachute -d 3 -i ${INPUTDIR}/in-C9-nVnG-fixedhuman-VREMAN-v12 -o out-C9-nVnG-fixedhuman-VREMAN-v12 &
+
