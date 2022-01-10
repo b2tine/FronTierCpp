a5a6c1ba07fc2866381501814c845b21483ac84b
brandon <brandon.ballentine@stonybrook.edu>
2022-01-10 14:47:04 -0500

    added script for 3d parallel bug in FrontPreAdvance() when pp_global_imax() is called with the integer variable max_body_number

---
+++ cairfoil/run-test-p112.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+
+MPIEXEC=/usr/local/pkg/petsc-3.13.4-dbg/bin/mpiexec
+#MPIEXEC=/opt/petsc-3.13.4-dbg/bin/mpiexec
+
+
+$MPIEXEC -np 2 ./cairfoil -d 3 -p 1 1 2 -i in-C9-Vent-Ball-vremansgs-ftsides-no-collsn-test -o out-test-p112 &
