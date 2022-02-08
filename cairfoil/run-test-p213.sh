14166b57df50b7c432a3555de8ac53e1c1b1d491
brandon <brandon.ballentine@stonybrook.edu>
2022-02-08 11:40:54 -0500

    run-test-p*.sh for testing mpi runs on workstations

--- cairfoil/run-test-p213.sh
+++ cairfoil/run-test-p213.sh
@@ -5,4 +5,4 @@ MPIEXEC=/usr/local/pkg/petsc-3.13.4-dbg/bin/mpiexec
 #MPIEXEC=/opt/petsc-3.13.4-dbg/bin/mpiexec
 
 
-$MPIEXEC -np 2 ./cairfoil -d 3 -p 1 1 2 -i $1 -o out-test-p112 &
+$MPIEXEC -np 6 ./cairfoil -d 3 -p 2 1 3 -i $1 -o out-test-p213 &
